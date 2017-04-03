54684cf8b22c4fd9efc9df0511ee7e3a2c60410f
MKS-Student <mks.student@hackreactor.com>
2017-04-03 13:41:38 -0700

    Sends a message

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,36 @@
-// YOUR CODE HERE:
+var app = {
+  init: function () {},
+  send: function (message) {
+    $.ajax({
+      url: "http://parse.sfs.hackreactor.com/chatterbox/classes/messages",
+      type: "POST",
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
