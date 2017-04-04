39098a03e070c80dbd735392e380a8531354cc42
MKS-Student <mks.student@hackreactor.com>
2017-04-03 21:32:30 -0700

    Partial implementation of dropdown box

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,10 @@
 var app = {
-  server: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
-  init: function () {},
+  server: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages?order=-createdAt',
+  allRooms: {},
+  init: function () {
+    app.fetch();
+    // console.log(app.allRooms) // undefined;
+  },
   
   send: function (message) {
     $.ajax({
@@ -18,37 +22,64 @@ var app = {
     });
   },
   fetch: function () {
+    // var dataObject = {
+    //   "roomname": roomName
+    // }
     $.ajax({
       url: this.server,
       type: 'GET',
       dataType: 'json',
       dataFilter: function(data) {
         var data = JSON.parse(data);
-        // var results = data.results;
+        // var.responseText = data.responseText;
         // loop through each result and look at the text
           // strip out the left bracket things
+        window.trash = data.results;
         for (var i = 0; i < data.results.length; i++) {
           // var re1 = /</;
           // var re2 = />/;
+          if (!data.results[i].text) {
+            data.results[i].text = ' ';
+          }
           var textToChange = data.results[i].text;
           var newText = textToChange.replace(/</g, 'java');
           var newText = newText.replace(/>/g, ' is fun! ');
           data.results[i].text = newText;
+
+          if (!data.results[i].username) {
+            data.results[i].username = ' ';
+          }
           var nameToChange = data.results[i].username;
           var newName = nameToChange.replace(/</g, 'java');
           var newName = newName.replace(/>/g, ' is fun! ');
           data.results[i].username = newName;
-          // use a regex to find the left brackets
+
+
         }
         return JSON.stringify(data);
       },
       success: function (data) {
         console.log('fetch successful');
         // prepend the data to the page
+
         for (var i = 0; i < data.results.length; i++) {
-          var chat = `<div class='chat'><p class='username'>${data.results[i].username}</p><p class='text'>${data.results[i].text}</p></div>`;
-          $('#chats').prepend(chat);
-          console.log(i);
+          var chat = data.results[i];
+
+          // take the message's room name and add it to a property so we can
+          if (data.results[i].roomname && app.allRooms[data.results[i].roomname] === undefined) {
+            app.allRooms[data.results[i].roomname] = [data.results[i]];
+          } else if (data.results[i].roomname) {
+            app.allRooms[data.results[i].roomname].push(data.results[i]);
+          }
+          // iterate over the property's keys to get all the possible room names
+        }
+
+        // set up our dropdown here;
+        var dropdown = $('#roomSelect');
+        for (var key in app.allRooms) {
+          var element = `<option value='${key}'>${key}</option>`;
+          dropdown.append(element);
+        // set up the input on the page
         }
 
         window.test = data.results;
@@ -57,16 +88,49 @@ var app = {
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to get messages', data);
+      },
+      complete: function () {
+        // var dropdown = $('#roomSelect');
+        // for (var key in app.allRooms) {
+        //   // set up the input on the page
+
+        // }
+
       }
     });
+  },
+  
+  clearMessages: function () {
+    $('#chats').children().remove();
+  },
+
+  renderMessage: function (message) {
+    var chat = `<div class='chat'><p class='username'>${message.username}</p><p class='text'>${message.text}</p></div>`;
+    $('#chats').prepend(chat);
+  },
+
+  renderRoom: function (roomName) {
+    $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages?order=-createdAt&where={"roomname":"' + roomName + '"}',
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        window.trash = data;
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+
+
   }
 };
 
 
-
-
-
-
+app.init();
 
 
 
