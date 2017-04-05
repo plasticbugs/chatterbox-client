be48cc574b5d6b414e8276bbed047e30f61a6263
MKS-Student <mks.student@hackreactor.com>
2017-04-04 17:23:56 -0700

    Some changes broke things

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,14 +2,39 @@ var app = {
   server: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages?order=-createdAt',
   allRooms: {},
   friends: {},
+  currentRoom: '',
   init: function () {
     app.fetch();
-    // console.log(app.allRooms) // undefined;
+    $(document).ready(function() {
+      if (!app.ranTheThing) {
+        $('#send .submit').on('submit', function (event) {
+          event.preventDefault(); 
+          app.handleSubmit();
+        });
+        app.ranTheThing = 1;
+      }
+      $('.new-room').on('click', function () {
+        var roomToCreate = prompt('What room name?') || 'lobby';
+        if (app.allRooms[roomToCreate] === undefined) {
+          app.allRooms[roomToCreate] = [];
+          var dropdown = $('#roomSelect');
+          var element = `<option value='${roomToCreate}'>${roomToCreate}</option>`;
+          dropdown.append(element);
+          $("select option[value='" + roomToCreate +"']").attr('selected','selected');
+        }
+        app.renderRoom(roomToCreate);
+      });
+    });
+    
+    setInterval(function () {
+      app.renderRoom(app.currentRoom);
+    }, 2500);
+
   },
   
   send: function (message) {
     $.ajax({
-      url: this.server,
+      url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
@@ -30,35 +55,7 @@ var app = {
       url: this.server,
       type: 'GET',
       dataType: 'json',
-      dataFilter: function(data) {
-        var data = JSON.parse(data);
-        // var.responseText = data.responseText;
-        // loop through each result and look at the text
-          // strip out the left bracket things
-        for (var i = 0; i < data.results.length; i++) {
-          // if there is no text in the message
-          if (!data.results[i].text) {
-            // splice out the empty message
-            data.results.splice(i,1);
-          } else {
-            var textToChange = data.results[i].text;
-            var newText = textToChange.replace(/</g, 'java');
-            var newText = newText.replace(/>/g, ' is fun! ');
-            data.results[i].text = newText;
-
-            if (!data.results[i].username) {
-              data.results[i].username = ' ';
-            }
-            var nameToChange = data.results[i].username;
-            var newName = nameToChange.replace(/</g, 'java');
-            var newName = newName.replace(/>/g, ' is fun! ');
-            data.results[i].username = newName;
-          }
-
-
-        }
-        return JSON.stringify(data);
-      },
+      // dataFilter: function (data) { return app.scrubData(data); },
       success: function (data) {
         console.log('fetch successful');
         // prepend the data to the page
@@ -81,28 +78,16 @@ var app = {
           dropdown.append(element);
         // set up the input on the page
         }
-        $( "select" ).change(function() {
-    //var str = "";
-          $( "select option:selected" ).each(function() {
+        $( 'select' ).change(function() {
+          $( 'select option:selected' ).each(function() {
             app.renderRoom($(this).text());
           });
-        //this calls render room
         }).trigger( "change" );
-        // set up the dropdown event listeners
-
         return true;
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to get messages', data);
-      },
-      complete: function () {
-        // var dropdown = $('#roomSelect');
-        // for (var key in app.allRooms) {
-        //   // set up the input on the page
-
-        // }
-
       }
     });
   },
@@ -112,13 +97,24 @@ var app = {
   },
 
   renderMessage: function (message) {
-    var chat = `<div class='chat'><a class='username' href="#">${message.username}</a><p class='text'>${message.text}</p></div>`;
+    var classes;
+    if (app.friends.hasOwnProperty(message.username)) {
+      // parse the chat string into a jquery obj
+      classes = 'username friend';
+    } else {
+      classes = 'username';
+    }
+    var chat = `<div class='chat'><a class='${classes}' href='#'>${message.username}</a><p class='text'>${message.text}</p></div>`;
     $('#chats').append(chat);
-    $('.username').on('click', app.handleUsernameClick);
+
+    $('.username').on('click', function(event) {
+      event.preventDefault();
+      app.handleUsernameClick.call(this);
+    });
   },
 
   renderRoom: function (roomName) {
-
+    app.currentRoom = roomName;
     if (!app.allRooms[roomName]) {
       var dropdown = $('#roomSelect');
       var element = `<option value='${roomName}'>${roomName}</option>`;
@@ -130,6 +126,7 @@ var app = {
       url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages?order=-createdAt&where={"roomname":"' + roomName + '"}',
       type: 'GET',
       contentType: 'application/json',
+      // dataFilter: function (data) { return app.scrubData(data); },
       success: function (data) {
         app.clearMessages();
         // loop through the data
@@ -139,7 +136,7 @@ var app = {
         });
 
 
-        console.log('chatterbox: Message sent');
+        console.log('chatterbox: Room Rendered');
       },
       error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -149,12 +146,48 @@ var app = {
   },
   handleUsernameClick: function () {
     app.friends[$(this).text()] = $(this).text();
-  }
+  },
+  handleSubmit: function () {
+    var message = {
+      username: window.location.search.slice(10),
+      text: $('#message').val(),
+      roomname: app.currentRoom
+    };
+    app.send(message);
+    $('#message').val('');
+  },
+  // scrubData: function (data) {
+
+  //   var data = JSON.parse(data);
+  //     // var.responseText = data.responseText;
+  //     // loop through each result and look at the text
+  //       // strip out the left bracket things
+  //   for (var i = 0; i < data.results.length; i++) {
+  //     // if there is no text in the message
+  //     if (!data.results[i].text) {
+  //       // splice out the empty message
+  //       data.results.splice(i, 1);
+  //     } else {
+  //       var textToChange = data.results[i].text;
+  //       var newText = textToChange.replace(/</g, 'java');
+  //       var newText = newText.replace(/>/g, ' is fun! ');
+  //       data.results[i].text = newText;
+
+  //       if (!data.results[i].username) {
+  //         data.results[i].username = ' ';
+  //       }
+  //       var nameToChange = data.results[i].username;
+  //       var newName = nameToChange.replace(/</g, 'java');
+  //       var newName = newName.replace(/>/g, ' is fun! ');
+  //       data.results[i].username = newName;
+  //     }
+  //   }
+  //   return JSON.stringify(data);
+  // }
 };
 
 
 app.init();
-
 
 
 
